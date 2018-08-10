9818552ca146ff6fe8a7d0213316b03e79e1ee5c
DaltonHart <daltonvaren@gmail.com>
2018-08-10 10:55:38 -0700

    created bat file

--- mybot.js
+++ mybot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 
 client.on("ready", () => {
   console.log("Let's get rolling!");
@@ -9,9 +9,12 @@ client.on("ready", () => {
 let selectedDice = "";
 
 client.on("message", (message) => {
-  if (message.content.startsWith("/d")) {
-    message.channel.send(`you rolled a d${selectedDice}!`);
+  if (message.content.startsWith(`${prefix}d6`)) {
+    message.channel.send(`you rolled a d6`);
+  } else if (message.content.startsWith(`${prefix}d4`)){
+    message.channel.send(`you rolled a d4`);
+
   }
 });
 
-client.login(config.token);
+client.login(token);
